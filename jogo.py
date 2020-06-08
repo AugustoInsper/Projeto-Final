99a091172bae247b686a688fe98a3411ceb2e318
Augusto <augustorr@al.insper.edu.br>
2020-06-08 17:23:17 -0300

    com arvore

--- jogo.py
+++ jogo.py
@@ -2,37 +2,42 @@
 # ----- Importa e inicia pacotes
 import pygame
 import random
-
+ 
 pygame.init()
-
+ 
 # ----- Gera tela principal
 WIDTH = 1000
 HEIGHT = 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Hello World!')
-
+ 
 # ----- Inicia assets
 BIRD_WIDTH = 125
 BIRD_HEIGHT = 103
+TREE_WIDTH = 108
+TREE_HEIGHT = 90
 font = pygame.font.SysFont(None, 48)
 background = pygame.image.load('source.gif').convert()
 background = pygame.transform.scale(background, (1000, 600))
-image = pygame.image.load('red_bird.gif').convert_alpha()
-image = pygame.transform.scale(image, (BIRD_WIDTH, BIRD_HEIGHT))
-
+bird = pygame.image.load('red_bird.gif').convert_alpha()
+bird = pygame.transform.scale(bird, (BIRD_WIDTH, BIRD_HEIGHT))
+tree = pygame.image.load('arvore.jpg').convert_alpha()
+tree = pygame.transform.scale(tree, (TREE_WIDTH, TREE_HEIGHT))
+ 
 # ----- Inicia estruturas de dados
 game = True
-# gravity = False
-
+ 
 bird_x = 250
 bird_y = 300
-#bird_speedx = 0
 bird_speedy = 0
-
+ 
+tree_x = 850
+tree_y = HEIGHT - TREE_HEIGHT
+ 
 # Variável para o ajuste de velocidade
 clock = pygame.time.Clock()
 FPS = 30
-
+ 
 # ===== Loop principal =====
 while game:
     # ----- Trata eventos
@@ -43,31 +48,20 @@ while game:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bird_speedy = -10
-#                gravity = True
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
                 bird_speedy = 10
-
-    # ----- Atualiza estado do jogo
-    # Atualizando a posição do player
-    
-    # Se o player passar do final da tela, volta para cima
-#    if gravity:
-        # bird_x = 250
-        # bird_y = -BIRD_HEIGHT
-        # bird_speedx = 0
-#        bird_speedy = 8
-
-    # bird_x += bird_speedx
-    bird_y += bird_speedy    
-
+ 
+    bird_y += bird_speedy
+ 
     # ----- Gera saídas
-    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.fill((255, 255, 255))  # Preenche com a cor branca
     window.blit(background, (0, 0))
-    window.blit(image, (bird_x, bird_y))
-
+    window.blit(bird, (bird_x, bird_y))
+    window.blit(tree, (tree_x, tree_y))
+ 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
-
+ 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
