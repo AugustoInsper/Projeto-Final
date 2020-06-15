2522b1c1f4806d29f6b118824bc1ad5e18c2869a
Breno Quessie <brenoqbl@al.insper.edu.br>
2020-06-15 19:50:40 -0300

    deu ruim

--- flepássaro.py
+++ flepássaro.py
@@ -2,7 +2,8 @@
 # ----- Importa e inicia pacotes
 import pygame
 import random
-
+import time
+ 
 pygame.init()
 pygame.mixer.init()
 
@@ -11,25 +12,25 @@ WIDTH = 1000
 HEIGHT = 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Flepássaro')
-
-# ----- Dimensões dos sprites
+ 
+# ----- Inicia assets
 BIRD_WIDTH = 100
 BIRD_HEIGHT = 82
 TREE_WIDTH = 108
 TREE_HEIGHT = HEIGHT
 
-# ----- Inicia assets
 assets = {}
-assets['tronco'] = pygame.image.load('imagens/tronco.png').convert_alpha()
-assets['tronco'] = pygame.transform.scale(assets['tronco'], (TREE_WIDTH, TREE_HEIGHT))
-background_anim = []
+# font = pygame.font.SysFont(None, 48)
 
-# No gif de onde foram pegos os seguintes frames, as árvores inicialmente estavam se movendo da esquerda para a direita,
-# o que não faz sentido ao analisar a ideia de movimento em relação ao player que queremos passar, por isso
-# tivemos que percorrer os frames de trás para frente
+assets['tronco'] = pygame.image.load('tronco.png').convert_alpha()
+assets['tronco'] = pygame.transform.scale(assets['tronco'], (TREE_WIDTH, TREE_HEIGHT))
+
+background_anim = []
+win_rect = window.get_rect()
 i=191
+
 while i>=0:
-    # Os arquivos de animação são numerados de 00 a 191
+    # Os arquivos de animação são numerados de 00 a 192
     filename = 'background_frames/frame{}-0000.jpg'.format(i)
     img = pygame.image.load(filename).convert()
     img = pygame.transform.scale(img, (int(WIDTH), int(HEIGHT)))
@@ -42,17 +43,15 @@ for i in range(73):
     # Os arquivos de animação são numerados de 00 a 72
     filename = 'red_bird_frames/oie_transparent ({}).png'.format(i)
     img = pygame.image.load(filename).convert_alpha()
-    img = pygame.transform.scale(img, (BIRD_WIDTH, BIRD_HEIGHT))
+    img = pygame.transform.scale(img, (BIRD_WIDTH, BIRD_HEIGHT))   # pegar dimensões do passarinho
     player_anim.append(img)
 assets["player_anim"] = player_anim
-assets["score_font"] = pygame.font.Font('fonte/PressStart2P.ttf', 28)
+assets["score_font"] = pygame.font.Font('PressStart2P.ttf', 28)
 
 
 # Carrega os áudios do Flepássaro
-pygame.mixer.music.load('Sons/background sound.wav')
+pygame.mixer.music.load('background sound.wav')
 pygame.mixer.music.set_volume(0.4)
-
-# Inicia classes
 
 class Player(pygame.sprite.Sprite):
     # Construtor da classe.
@@ -89,7 +88,7 @@ class Player(pygame.sprite.Sprite):
                 self.rect = self.image.get_rect()
                 self.rect.center = center
 
-        # Não permite que o player passe para cima e para baixo da janela
+
         self.rect.y += self.speedy
         if self.rect.bottom > HEIGHT:
             self.rect.bottom = HEIGHT
@@ -125,13 +124,14 @@ class Background(pygame.sprite.Sprite):
             if self.frame == len(self.background_anim):
                 self.frame = 0
             else:
+                # Se ainda não chegou ao fim da explosão, troca de imagem.
                 center = self.rect.center
                 self.image = self.background_anim[self.frame]
                 self.rect = self.image.get_rect()
                 self.rect.center = center
 
 
-class Treeup(pygame.sprite.Sprite):             # Classe dos troncos que vão aparecer na parte de cima da tela
+class Treeup(pygame.sprite.Sprite):
     def __init__(self,assets):
         pygame.sprite.Sprite.__init__(self)
 
@@ -146,7 +146,7 @@ class Treeup(pygame.sprite.Sprite):             # Classe dos troncos que vão ap
         self.rect.x += self.speedx
 
 
-class Treedown(pygame.sprite.Sprite):           # Classe dos troncos que vão aparecer na parte de baixo da tela
+class Treedown(pygame.sprite.Sprite):
     def __init__(self,assets):
         pygame.sprite.Sprite.__init__(self)
 
@@ -160,22 +160,30 @@ class Treedown(pygame.sprite.Sprite):           # Classe dos troncos que vão ap
     def update(self):
         self.rect.x += self.speedx
 
-
-# Inicia funções
 
 def menu(screen):
+    # Variável para o ajuste de velocidade
+    # clock = pygame.time.Clock()
+    # game = True
+
     # Carrega o fundo da tela inicial
-    inicio = pygame.image.load('imagens/Capa_flepassaro.jpg').convert()
+    inicio = pygame.image.load('Capa_flepassaro.jpg').convert()
     inicio = pygame.transform.scale(inicio, (1000, 389))
     inicio_rect = inicio.get_rect()
 
     inits = True
+
     while inits:
-        # Processa os eventos
+        # Ajusta a velocidade do jogo.
+    #    clock.tick(FPS)
+
+        # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get():
+            # Verifica se foi fechado.
             if event.type == pygame.QUIT:
                 inits = False
                 state = CLOSE
+
             if event.type == pygame.KEYUP:
                 inits = False
                 state = GAME
@@ -184,24 +192,31 @@ def menu(screen):
         screen.fill((0, 0, 0))
         screen.blit(inicio, (0, HEIGHT/2 - int(389/2)))
 
+        # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
+
     return state
 
 
 def gameover(screen): 
     # Carrega o fundo da tela inicial
-    fim = pygame.image.load('imagens/perdeu.jpg').convert()
+    fim = pygame.image.load('perdeu.jpg').convert()
     fim = pygame.transform.scale(fim, (WIDTH, HEIGHT))
     fim_rect = fim.get_rect()
 
     finals = True
 
     while finals:
+        # Ajusta a velocidade do jogo.
+    #    clock.tick(FPS)
+
+        # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get():
-            # Processa os eventos
+            # Verifica se foi fechado.
             if event.type == pygame.QUIT:
                 finals = False
                 state = CLOSE
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_ESCAPE:
                     finals = False
@@ -211,19 +226,21 @@ def gameover(screen):
         screen.fill((0, 0, 0))
         screen.blit(fim, fim_rect)
 
-        # Escreve a pontuação na tela de Game Over
-        text_surface = assets['score_font'].render("{:00d}".format(score), True, (0, 0, 0))
-        text_rect = text_surface.get_rect()
-        text_rect.midtop = (WIDTH / 2,  10)
-        window.blit(text_surface, text_rect)
-
+        # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
+
     return state
 
 
 # ----- Inicia estruturas de dados
+ 
 bird_x = 250
 bird_y = 200
+bird_speedy = 0
+
+# Variável para o ajuste de velocidade
+clock = pygame.time.Clock()
+FPS = 30
 
 all_sprites = pygame.sprite.Group()
 all_trees = pygame.sprite.Group()
@@ -252,13 +269,14 @@ player_speedy = 1.3
 GAME = 0
 CLOSE = 1
 state = menu(window)
-pygame.mixer.music.play(loops=-1)
 
 # ===== Loop principal =====
-while state == GAME:
+pygame.mixer.music.play(loops=-1)
 
+while state == GAME:
+    
     # Se a árvore 1 chega em determinado ponto x, é criada a ávore 2:
-    if treeup1.rect.x == 650:
+    if treeup1.rect.x == 600:
         treeup2 = Treeup(assets)
         treedown2 = Treedown(assets)
 
@@ -276,9 +294,9 @@ while state == GAME:
 
         tup2 = True
 
-    # Se a ávore 2 chega em determinado ponto x, a árvore 1 é recriada:
+# Se a ávore 2 chegar em determinado ponto x, a árvore 1 é recriada:
     if tup2:
-        if treeup2.rect.x == 650:
+        if treeup2.rect.x == 700:
             treeup1 = Treeup(assets)
             treedown1 = Treedown(assets)
 
@@ -297,7 +315,6 @@ while state == GAME:
         if treeup1.rect.x == bird_x or treeup2.rect.x == bird_x:
             score += 1
 
-    # Casos em que o player perde o jogo:
     hits = pygame.sprite.spritecollide(player, all_trees, False, pygame.sprite.collide_mask)
     if len(hits) > 0:
         state = CLOSE
@@ -334,12 +351,7 @@ while state == GAME:
     text_rect.midtop = (WIDTH / 2,  10)
     window.blit(text_surface, text_rect)
 
-    # Se o estado é "CLOSE", começa a nova música para a tela de Game Over e chama a classe responsável por essa parte do jogo
     if state == CLOSE:
-        pygame.mixer.music.stop()
-        pygame.mixer.music.load('Sons/game over.mp3')
-        pygame.mixer.music.set_volume(0.4)
-        pygame.mixer.music.play(loops = 0)
 
         gameover(window)
 
@@ -348,3 +360,5 @@ while state == GAME:
  
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
+ 
